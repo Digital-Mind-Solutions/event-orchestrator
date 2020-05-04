2a0b7424e10df943a8c2e01ac70bd167f179e06f
Lucian Dragomir <lucian.dragomir@vodafone.com>
2020-05-04 21:26:10 +0300

    - add clear cause message on event orchestrator exceptions

--- src/main/java/org/digitalmind/eventorchestrator/utils/EventOrchestratorExceptionUtils.java
+++ src/main/java/org/digitalmind/eventorchestrator/utils/EventOrchestratorExceptionUtils.java
@@ -2,8 +2,10 @@ package org.digitalmind.eventorchestrator.utils;
 
 import org.digitalmind.eventorchestrator.exception.EventDirectiveException;
 import org.digitalmind.eventorchestrator.exception.EventOrchestratorException;
+import org.springframework.expression.ExpressionInvocationTargetException;
 
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 
 public class EventOrchestratorExceptionUtils {
 
@@ -16,6 +18,12 @@ public class EventOrchestratorExceptionUtils {
             cause = cause.getCause();
         }
         if (cause instanceof EventOrchestratorException) {
+            cause = cause.getCause();
+        }
+        if (cause instanceof ExpressionInvocationTargetException) {
+            cause = cause.getCause();
+        }
+        if (cause instanceof ExecutionException) {
             cause = cause.getCause();
         }
         if (cause instanceof CompletionException) {
